110a2f567bcd03ab3e3eeda9acdb7788bdffe32f
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-03-07 12:12:43 +0100

    Remove junk files

--- es02/app.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('hello world');
-    res.end();
-});
-server.listen(8080);
