ef4c2031416134d3e9df209fb94512a5cfe72c78
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-03-07 13:00:45 +0100

    Complete es03

---
+++ es03/routes.js
@@ -0,0 +1,24 @@
+const requestHandler = (req, res) => {
+    //implementare qui la logica di gestione della richiesta e della risposta
+    if (req.url === '/' && req.method === 'GET') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<html lang="it">');
+        res.write('<head><title>Inserisci messaggio</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button>INVIA</button></form></body>');
+        res.write('</html>');
+        res.end();
+    } else if (req.url === '/message' && req.method === 'POST') {
+        const body = [];
+        req.on('data', chunk => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+        });
+        res.end();
+    }
+}
+
+//esportare qui la funzione implementata
+exports.handler = requestHandler;
